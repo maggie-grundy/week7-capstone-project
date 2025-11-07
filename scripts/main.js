3811e54684728af5b3cbec9355819343464d5405
Maggie Grundy <maggie.grundy@gmail.com>
2025-11-06 19:19:01 -0600

    Troubleshoot location/date populating (not working still) and error states (sort of working)

--- scripts/main.js
+++ scripts/main.js
@@ -20,19 +20,22 @@ async function fetchCityData(cityName) {
     const geoData = await geoResponse.json();
     if (!geoData.results?.length) throw new Error("City not found");
 
-    const { latitude, longitude } = geoData.results[0];
-    await fetchForecast(latitude, longitude);
+    const { latitude, longitude, name, country } = geoData.results[0];
+    await fetchForecast(latitude, longitude, name, country);
   } catch (error) {
-    console.error(error);
+    console.error(error); 
+    /*Replace form w/ 404 */
+    weatherForm.innerHTML = "<p style='color:red;'>404: City not found!</p>";
   }
 }
 
-async function fetchForecast(lat, lon) {
+async function fetchForecast(lat, lon, cityName, country) {
   try {
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,apparent_temperature,precipitation,wind_speed_10m,relative_humidity_2m&timezone=auto`;
     const response = await fetch(url);
     const data = await response.json();
-    updateWeatherUI(data.current);
+
+    updateWeatherUI(data.current, cityName, country);
   } catch (error) {
     console.error(error);
   }
@@ -47,25 +50,13 @@ function updateWeatherUI(current) {
   document.getElementById("humidityValue").textContent = `Humidity: ${current.relative_humidity_2m}%`;
   document.getElementById("windValue").textContent = `Wind: ${current.wind_speed_10m} m/s`;
   document.getElementById("precipValue").textContent = `Precipitation: ${current.precipitation} mm`;
- 
- 
 
-  //Populate location header
-    const { cityname, country } = geoData.results[0];
-   document.getElementById("resultsLocation)").textContent = `${cityname}, ${country}`;
-
-   //Define & populate current date
-  const now = new Date();
-    document.getElementById("resultsDate").textcontent = now.toLocaleDateString("en-us", {
-    weekday: "long",
-    month: "long",
-    day: "numeric"
-});
-
-
-
+  document.getElementById("resultsLocation").textContent = `${cityName}, ${country}`;
+  document.getElementById("resultsDate").textContent = new Date().toLocaleDateString();
 
 }
+
+
 
 
 
